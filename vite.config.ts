b98ac0ce8df75b5c8b7bd9d0839aa67622b214d2
Peeyush <peeyush.delta4infotech@gmail.com>
2026-04-20 15:05:23 +0530

    fix: update YourGPT script source to local and configure proxy in Vite

--- vite.config.ts
+++ vite.config.ts
@@ -13,4 +13,13 @@ export default defineConfig({
       '@': path.resolve(__dirname, './src'),
     },
   },
+  server: {
+    port: 3000,
+    proxy: {
+      '/script_local.js': {
+        target: 'http://192.168.18.169:4173',
+        changeOrigin: true,
+      }
+    }
+  },
 })
